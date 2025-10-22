d8c682125243ca5beef57ec8030cf44c3affd390
viranja963 <as20240963@sci.sjp.ac.lk>
2025-10-22 10:57:04 +0530

    Adding constants in the program

---
+++ Logistics_management_System/src/logistics_management_system/Logistics_management_System.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package logistics_management_system;
+
+/**
+ *
+ * @author acer
+ */
+public class Logistics_management_System {
+
+    /**
+     * @param args the command line arguments
+     */
+     private static final int MAX_CITIES = 30;
+    private static final int MAX_DELIVERIES = 50;
+    private static final double FUEL_PRICE = 310.0;
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
